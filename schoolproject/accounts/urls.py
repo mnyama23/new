84e388a708a56ccca68d168b07790828531e770b
mnyama23 <colleraoramys@gmail.com>
2026-06-03 12:51:28 -0700

    Update urls.py

--- schoolproject/accounts/urls.py
+++ schoolproject/accounts/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from .views import login_view
+from .views import next
 
 urlpatterns = [
     path("", login_view, name="login"),
+    path("continue", next, name="next"),
 ]
